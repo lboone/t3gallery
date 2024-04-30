01c5dae61c1bb69450103971ab76589645d7cce5
Lloyd Boone <lboone@progressive.com>
2024-04-30 15:09:29 -0400

    authentication with clerk added

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,20 @@
+import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const TopNav = () => {
+  return (
+    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
+        <div>Gallery</div>
+        <div>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </div>
+    </nav>
+  )
+}
+
+export default TopNav
