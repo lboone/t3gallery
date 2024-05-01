9ce36f9a0fe0995954a837f69c44c25071878eb0
Lloyd Boone <lboone@progressive.com>
2024-05-01 16:46:24 -0400

    added new icon for upload complete toast

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -29,7 +29,39 @@ const useUploadThingInputProps = (...args: Input) => {
     isUploading: $ut.isUploading,
   };
 };
+function InfoSVG(){
+    return(
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            strokeWidth={1.5} 
+            stroke="white" 
+            className="w-6 h-6">
+            <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" 
+            />
+        </svg>
+    ) 
+}
 
+function InfoSVGSolid(){
+    return(
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24" 
+            fill="currentColor" 
+            className="w-6 h-6">
+            <path 
+                fillRule="evenodd" 
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" 
+                clipRule="evenodd" 
+            />
+        </svg>
+    )
+}
 function UploadSVG(){
     return(
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -94,7 +126,11 @@ export function SimpleUploadButton(){
         },
         onClientUploadComplete: () => {
             toast.dismiss("upload-begin");
-            toast("Upload complete!");
+            toast(
+                <div className="flex gap-2 items-center">
+                    <InfoSVGSolid /><span className="text-lg">Upload Complete!</span>
+                </div>
+            );
             router.refresh();
         }
     });
