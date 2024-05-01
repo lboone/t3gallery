78ce2fa5b21d8ed239785bba508c2eeca09fc90f
Lloyd Boone <lboone@progressive.com>
2024-05-01 14:01:00 -0400

    move to the grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,12 +33,15 @@ export default function RootLayout({
         <NextSSRPlugin 
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-        <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
-          <TopNav />
-          {children}
+        <body className={`font-sans ${inter.variable}`}>
+          <div className="h-screen grid grid-rows-[auto,1fr]">
+            <TopNav />
+            <div className="overflow-y-scroll">{children}</div>
+          </div>
           {modal}
           <div id="modal-root" />
         </body>
+
       </html>
     </ClerkProvider>
   );
